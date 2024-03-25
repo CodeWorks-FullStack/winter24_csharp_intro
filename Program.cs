1f2a2703934cadb111ebc0dbc98459ec8f0c9911
Jeremy Fowler <jembyfowler@gmail.com>
2024-03-25 12:53:29 -0600

    added notes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using System.Numerics;
 
 Console.Clear();
 
 Console.WriteLine("Hello, World!");
 
+// SECTION strings
 
 string name = "Jeremy";
 
@@ -17,6 +17,12 @@ char letter = 'c';
 Console.WriteLine($"My name is {name} and my favorite food is {favoriteFood}");
 
 Console.WriteLine(""""" curly "boyz" """"".Trim());
+
+// !SECTION
+
+
+
+// SECTION booleans and conditionals
 
 bool jeremyHasManyCats = true;
 
@@ -44,6 +50,11 @@ if (name != "Jeremy")
   Console.WriteLine("Name was not Jeremy");
 }
 
+// !SECTION
+
+
+
+// SECTION numbers
 
 int numberOfCats = 6;
 
@@ -55,38 +66,43 @@ double decimalKinda = 7.35;
 
 decimal superAccurateDecimal = 6.82641209843922342355234M; // NOTE probably be used for money application
 
-
 Console.WriteLine(25 + 3 / 100 * 3);
+
+
 double total = numberOfCats + decimalKinda;
+
 Console.WriteLine(total);
 
-Cat gopher = new Cat("Gopher", 4, "Black", true, 8);
+// !SECTION
 
+// SECTION objects, arrays, loops
 
-var georgie = new Cat("Georgie", 4, "Grey", true, 6);
+Cat gopher = new Cat("Gopher", 4, "Black", true, 8); // explicit type
 
-var nameOfCat = georgie.Name;
+var georgie = new Cat("Georgie", 4, "Grey", true, 6); // implicit type
 
+var nameOfCat = georgie.Name; // implicit type
 
-int[] numbers = [1, 2, 3, 99];
+int[] numbers = [1, 2, 3, 99]; // arrays are stinky in C#
 
 for (int i = 0; i < numbers.Length; i++)
 {
   Console.WriteLine($"numbers at index of {i} is {numbers[i]}");
 }
 
-List<string> names = ["jeremy", "mick", "jake"];
+List<string> names = ["jeremy", "mick", "jake"]; // Lists will be the collection data type we use the most
 
-foreach (var firstName in names)
+foreach (var firstName in names) // foreach runs similar to for in loop from js
 {
   Console.WriteLine(firstName);
 }
 
-names.ForEach(firstName => Console.WriteLine(firstName));
+names.ForEach(firstName => Console.WriteLine(firstName)); // ForEach is a List Method that runs similar to forEach from js
 
-var cats = new List<Cat> { new Cat("Cybil", 4, "Calico", false, 9) };
 
-cats.Add(gopher);
+var cats = new List<Cat> { new Cat("Cybil", 4, "Calico", false, 9) }; // creates a List of type Cat
+
+cats.Add(gopher); // adds to end of list, similar to push from js
 cats.Add(georgie);
 
 foreach (var cat in cats)
@@ -99,18 +115,25 @@ foreach (var cat in cats)
 
 // }
 
-Cat? foundCat = cats.Find(cat => cat.Name == "Gopher");
+Cat? foundCat = cats.Find(cat => cat.Name == "Gopher"); // finds a single Cat from list, ? denotes that foundCat can be null
 
 
 class Cat
 {
+  // NOTE properties that exist on Cat class
+  // get set allows us to view / change properties on data
+  // public is an access modifier that allows any outside code to access this property
   public string Name { get; set; }
   public int NumberOfLegs { get; set; }
   public string Color { get; set; }
   public bool LikesCheese { get; set; }
+  // NOTE private members are only accessible to the code inside this class
   private int NumberOfLives;
+
+  // NOTE getter method, can access private members of class
   public bool IsAlive { get { return NumberOfLives > 0; } }
 
+  // NOTE method, string is return type
   public string IntroduceYourself()
   {
     return $@"
@@ -121,6 +144,7 @@ class Cat
     ";
   }
 
+  // NOTE constructor
   public Cat(string name, int numberOfLegs, string color, bool likesCheese, int numberOfLives)
   {
     Name = name;
@@ -141,3 +165,5 @@ class Cat
 //     private int NumberOfLives = numberOfLives;
 //   public bool IsAlive { get { return NumberOfLives > 0; } }
 // }
+
+// !SECTION
